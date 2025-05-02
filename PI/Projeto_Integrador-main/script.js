358a1fc8dd7e9967e03575f8e16e1fe78f0ed07e
FelipeAmorim09 <felipe200872@outlook.com>
2025-05-02 00:32:03 -0300

    pagina-principal

---
+++ PI/Projeto_Integrador-main/script.js
@@ -0,0 +1,14 @@
+
+
+// Adicione este script
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    // Adicione aqui a lógica para abrir/fechar seu menu
+    // Exemplo: document.querySelector('nav').classList.toggle('show');
+});
+
+
+
+
+
+
